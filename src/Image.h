514bcf0e1e498b919e490ff1d20e8ffc9d5c8659
zijieh4 <hzj010427@gmail.com>
2023-11-12 04:08:06 +0800

    update data type

--- src/Image.h
+++ src/Image.h
@@ -10,9 +10,9 @@
 typedef struct {
 	unsigned int W;	   /* image width */
 	unsigned int H;    /* image height */
-	unsigned char *R;  /* pointer to the memory storing all the R intensity values */
-	unsigned char *G;  /* pointer to the memory storing all the G intensity values */
-	unsigned char *B;  /* pointer to the memory storing all the B intensity values */
+	double *R;  /* pointer to the memory storing all the R intensity values */
+	double *G;  /* pointer to the memory storing all the G intensity values */
+	double *B;  /* pointer to the memory storing all the B intensity values */
 } Image;
 
 /* Get the R intensity of pixel (x, y) in image */
@@ -25,13 +25,13 @@ double GetPixelG(const Image *image, unsigned int x,  unsigned int y);
 double GetPixelB(const Image *image, unsigned int x,  unsigned int y);
 
 /* Set the R intensity of pixel (x, y) in image to r */
-void SetPixelR(Image *image, unsigned int x,  unsigned int y, unsigned char r);
+void SetPixelR(Image *image, unsigned int x,  unsigned int y, double r);
 
 /* Set the G intensity of pixel (x, y) in image to g */
-void SetPixelG(Image *image, unsigned int x,  unsigned int y, unsigned char g);
+void SetPixelG(Image *image, unsigned int x,  unsigned int y, double g);
 
 /* Set the B intensity of pixel (x, y) in image to b */
-void SetPixelB(Image *image, unsigned int x,  unsigned int y, unsigned char b);
+void SetPixelB(Image *image, unsigned int x,  unsigned int y, double b);
 
 /* Allocate dynamic memory for the image structure and its R/G/B values */
 /* Return the pointer to the image, or NULL in case of error */
