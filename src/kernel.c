de5150c84b7bbe0ad37b5dcb7b58f17b17fecda5
zijieh4 <hzj010427@gmail.com>
2023-11-11 11:58:52 +0800

    update reset ymm reg

--- src/kernel.c
+++ src/kernel.c
@@ -43,6 +43,9 @@ Image *MotionBlur_Turbo(Image *image) {
     double   blurR[TOTAL_PIXELS];
 	double   blurG[TOTAL_PIXELS];
 	double   blurB[TOTAL_PIXELS];
+    double   resultR[TOTAL_PIXELS];
+    double   resultG[TOTAL_PIXELS];
+    double   resultB[TOTAL_PIXELS];
 
     double *tmpR = (double *)malloc(TMP_TOTAL_PIXELS * sizeof(double));
     double *tmpG = (double *)malloc(TMP_TOTAL_PIXELS * sizeof(double));
@@ -76,18 +79,18 @@ Image *MotionBlur_Turbo(Image *image) {
     printf("check3\n");
 
     /* kernel */
-    kernel_blur(tmpR, blurR, TMP_TOTAL_PIXELS);
-    kernel_blur(tmpG, blurG, TMP_TOTAL_PIXELS);
-    kernel_blur(tmpB, blurB, TMP_TOTAL_PIXELS);
+    kernel_blur(tmpR, resultR, TMP_TOTAL_PIXELS);
+    kernel_blur(tmpG, resultG, TMP_TOTAL_PIXELS);
+    kernel_blur(tmpB, resultB, TMP_TOTAL_PIXELS);
 
     printf("check4\n");
 
     /* set the RGB */
 	for (y = 0; y < HEIGHT; y++){
 		for (x = 0; x < WIDTH; x++) {
-            SetPixelR(image, x, y, blurR[x + y * WIDTH]);
-			SetPixelG(image, x, y, blurG[x + y * WIDTH]);
-			SetPixelB(image, x, y, blurB[x + y * WIDTH]);
+            SetPixelR(image, x, y, resultR[x + y * WIDTH]);
+			SetPixelG(image, x, y, resultG[x + y * WIDTH]);
+			SetPixelB(image, x, y, resultB[x + y * WIDTH]);
 		}
 	}
 
@@ -110,10 +113,15 @@ void pre_process_blur(double* input, double* output, int numPixels) {
     __m256d ymm0, ymm1, ymm2, ymm3;
     __m256d ymm4, ymm5, ymm6, ymm7;
     __m256d ymm8, ymm9, ymm10, ymm11;
-    printf("Entering pre_process_blur, numPixels: %d\n", numPixels);
 
     for (int i = 0; i < numPixels / 4; i += 4) {
-        printf("Loop iteration i: %d\n", i);
+        ymm0 = _mm256_setzero_pd();  ymm1 = _mm256_setzero_pd();
+        ymm2 = _mm256_setzero_pd();  ymm3 = _mm256_setzero_pd();
+        ymm4 = _mm256_setzero_pd();  ymm5 = _mm256_setzero_pd();
+        ymm6 = _mm256_setzero_pd();  ymm7 = _mm256_setzero_pd();
+        ymm8 = _mm256_setzero_pd();  ymm9 = _mm256_setzero_pd();
+        ymm10 = _mm256_setzero_pd();  ymm11 = _mm256_setzero_pd();
+
         ymm0 = _mm256_loadu_pd(&input[i]);
         ymm1 = _mm256_loadu_pd(&input[i+4]);
         ymm2 = _mm256_loadu_pd(&input[i+8]);
@@ -128,13 +136,11 @@ void pre_process_blur(double* input, double* output, int numPixels) {
         ymm10 = _mm256_shuffle_pd(ymm6, ymm7, 0x0);
         ymm11 = _mm256_shuffle_pd(ymm6, ymm7, 0xf);
 
-        printf("Loop iteration 4 * i: %d\n", 4 * i);
         _mm256_storeu_pd(&output[i*4], ymm8);
         _mm256_storeu_pd(&output[i*4+4], ymm9);
         _mm256_storeu_pd(&output[i*4+8], ymm10);
         _mm256_storeu_pd(&output[i*4+12], ymm11);
     }
-    printf("Exiting pre_process_blur\n");
 }
 
 /**
@@ -149,6 +155,14 @@ void kernel_blur(double* input, double* output, int numPixels) {
     __m256d ymm6, ymm7, ymm8, ymm9, ymm10, ymm11, ymm12, ymm13;
 
     for (int j = 0; j < (numPixels / 44); j += 44) {
+        ymm0 = _mm256_setzero_pd();  ymm1 = _mm256_setzero_pd();
+        ymm2 = _mm256_setzero_pd();  ymm3 = _mm256_setzero_pd();
+        ymm4 = _mm256_setzero_pd();  ymm5 = _mm256_setzero_pd();
+        ymm6 = _mm256_setzero_pd();  ymm7 = _mm256_setzero_pd();
+        ymm8 = _mm256_setzero_pd();  ymm9 = _mm256_setzero_pd();
+        ymm10 = _mm256_setzero_pd();  ymm11 = _mm256_setzero_pd();
+        ymm12 = _mm256_setzero_pd();  ymm13 = _mm256_setzero_pd();
+
         ymm0 = _mm256_set_pd(1/2, 1/2, 1/2, 1/2);
         ymm1 = _mm256_set_pd(1/6, 1/6, 1/6, 1/6);
 
