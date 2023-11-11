c9d46df7b1506a1a721af64ff3d690fd9a5dafcb
zijieh4 <hzj010427@gmail.com>
2023-11-12 04:05:25 +0800

    add post process blur

--- src/kernel.h
+++ src/kernel.h
@@ -18,5 +18,8 @@ Image *MotionBlur_Turbo(Image *image);
 /* Pre-process the image for motion blur */
 void pre_process_blur(double* input, double* output, int numPixels);
 
+/* Post-process the image for motion blur */
+void post_process_blur(double* input, double* output, int numPixels);
+
 /* Kernel for motion blur */
 void kernel_blur(double* input, double* output, int numPixels);
